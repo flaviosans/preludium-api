23657e143df8a5d2e52439179c8fde3ddd3398e2
euflaviosantos <flawinhoh@gmail.com>
2018-12-03 10:19:05 -0200

    Ignorando componentes de front

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -9,8 +9,8 @@
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
-</head>
+<link rel="stylesheet" href="styles.bb3f696ffec1679b8bf8.css"></head>
 <body>
   <app-root></app-root>
-<script type="text/javascript" src="assets/runtime.js"></script><script type="text/javascript" src="assets/polyfills.js"></script><script type="text/javascript" src="assets/styles.js"></script><script type="text/javascript" src="assets/vendor.js"></script><script type="text/javascript" src="assets/main.js"></script></body>
+<script type="text/javascript" src="runtime.a66f828dca56eeb90e02.js"></script><script type="text/javascript" src="polyfills.7fb637d055581aa28d51.js"></script><script type="text/javascript" src="main.4e915dd1ade0795ef7b5.js"></script></body>
 </html>
